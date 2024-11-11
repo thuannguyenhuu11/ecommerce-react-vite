12941101b0a1c88d1a7c9fd9f91cbc562a0ccec8
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-11 18:36:21 +0700

    feat: config axios

---
+++ src/apis/productsService.js
@@ -0,0 +1,9 @@
+import axiosClient from './axiosClient';
+
+const getProducts = async () => {
+    const res = await axiosClient.get('/product');
+
+    console.log(res);
+};
+
+export { getProducts };
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,9 +4,15 @@ import styles from './styles.module.scss';
 import Info from '@/components/Info/Info';
 import AdvancedHeadling from '@components/AdvancedHeadling/AdvancedHeadling';
 import HeadlingListProducts from '@components/HeadlingListProducts/HeadlingListProducts';
+import { useEffect } from 'react';
+import { getProducts } from '@/apis/productsService';
 
 const HomePage = () => {
     const { container } = styles;
+
+    useEffect(() => {
+        getProducts();
+    }, []);
 
     return (
         <div>
