e57ac102e9f43c085c894d6ecbf02b0b8fffe878
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-11 18:14:55 +0700

    feat: update list product section

--- src/components/HeadlingListProducts/HeadlingListProducts.jsx
+++ src/components/HeadlingListProducts/HeadlingListProducts.jsx
@@ -1,18 +1,18 @@
 import MainLayout from '@components/Layout/Layout';
 import CountdownBanner from '@components/CountDownBanner/CountDownBanner';
 import styles from './styles.module.scss';
+import ProductItem from '@components/ProductItem/ProductItem';
 
 const HeadlingListProducts = () => {
     const { container, containerItem } = styles;
 
     return (
         <MainLayout>
-            {/* <CountdownTimer targetDate={targetDate} /> */}
             <div className={container}>
                 <CountdownBanner />
                 <div className={containerItem}>
-                    <div>1</div>
-                    <div>2</div>
+                    <ProductItem />
+                    <ProductItem />
                 </div>
             </div>
         </MainLayout>
