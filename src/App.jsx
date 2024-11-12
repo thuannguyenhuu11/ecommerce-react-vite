60f8d3d02b5a89edbc8b0e17a6ca3dabe4ef0aa9
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-12 08:39:01 +0700

    feat: config react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import HomePage from '@/components/HomePage/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routers from '@/routers/routers';
+import { Suspense } from 'react';
 
 function App() {
     return (
         <>
-            <HomePage />
+            <BrowserRouter>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        {routers.map((item, index) => {
+                            return (
+                                <Route
+                                    path={item.path}
+                                    element={<item.component />}
+                                    key={index}
+                                />
+                            );
+                        })}
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
         </>
     );
 }
---
+++ src/routers/routers.js
@@ -0,0 +1,18 @@
+import { lazy } from 'react';
+
+const routers = [
+    {
+        path: '/',
+        component: lazy(() => import('@components/HomePage/HomePage'))
+    },
+    {
+        path: '/blog',
+        component: lazy(() => import('@components/Blog/Blog'))
+    }
+    // {
+    //     path: '/shop',
+    //     component: lazy(() => import('@pages/OurShop/OurShop'))
+    // }
+];
+
+export default routers;
